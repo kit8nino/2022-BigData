9c3c6a528243bad5f40a5ad77f52095df342515b
Nelliel2 <anna20025612@mail.ru>
2022-11-27 09:17:23 +0300

    [3] lab

---
+++ Смирнова Анна Николаевна/_lab-3/lab3.py
@@ -0,0 +1,44 @@
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+f = open('result.csv', 'w', newline='')
+file_writer = csv.writer(f, delimiter=',', quotechar='"')
+try:
+  link = 'https:///royallib.com/genre/detskoe/'
+  options = Options()
+  options.add_experimental_option('prefs', {
+    'download.default_directory': r'D:\Test',
+    'download.prompt_for_download': False,
+    'download.directory_upgrade': True,
+    'safebrowsing.enabled': True
+  })
+  drv = webdriver.Chrome(options=options)
+  drv.get(link)
+  srch = drv.find_elements(by='xpath', value='//a[starts-with(@href, "//royallib.com/genre/detskoe-")]')
+  granre_links = [elem.get_attribute('href') for elem in srch]
+  print(granre_links)
+  books = {}
+  for i in granre_links:
+      drv.get(i)
+      srch = drv.find_elements(by='xpath', value='//a[starts-with(@href, "//royallib.com/book/")]')
+      name_links = [elem.get_attribute('href') for elem in srch]
+      print(name_links)
+      for l in name_links:
+        try:
+          drv.get(l)
+          descript = drv.find_element(by='xpath', value='/html/body/div[2]/div/div[2]/div[2]/div/table[1]/tbody/tr/td[2]/table').text.split('\n')
+          year=''
+          for line in descript:
+            if line.startswith('Название:'):
+                book_name = line[10:]
+            if line.startswith('Год издания:'):
+                year = line[13:]
+                break
+          file_writer.writerow([f'"{book_name}", "{year}"'])
+        except:
+          continue
+finally:
+   f.close()
+   drv.quit()
+   input('press Enter')
