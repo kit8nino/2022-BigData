350e03008d4e4d807978e2955cfafa477b3e72d7
Хазова Александра <72195557+Khazovalexandra@users.noreply.github.com>
2022-12-06 12:34:35 +0300

    [4] пока что так

--- Хазова Александра Сергеевна/лаборатория 4/4.py
+++ Хазова Александра Сергеевна/лаборатория 4/4.py
@@ -1,28 +1,38 @@
 from functools import reduce
 import numpy as np
 import pandas as pd
+import itertools
 #import matplotlib.pyplot as plt
 
 data_ = []
 m = []
 
-for l in open(r'Хазова Александра Сергеевна\lab_3\books.csv', 'r', encoding="utf8").readlines()[1:]:
+mapped = open('mapped.csv', 'w', encoding='utf-8')
+
+for l in open(r'C:\Users\Сергей\Documents\parsing\books.csv', 'r', encoding="utf8").readlines()[1:]:
     data_.append(l.split(',')[0])
-    for w in data_:
-        words = w.split(' ')
-        for u in words:
-            m.append(u)
+#data = itertools.islice(data_, 100) 
+for w in data_:
+    words = w.split(' ')
+    for u in words:
+        #m.append(u)
+        mapped.write(u + '\n')
+
+mapped.close()
 
 res = [] 
-res = m
-#print(m)
+for r in open(r'C:\Users\Сергей\Documents\parsing\mapped.csv', 'r', encoding='utf-8').readlines():
+    res.append(r.split('\n')[0])
+
+res = tuple(res)
 
 data = list(map(lambda x: (x, 1), res))
 data.sort()
-#print(data)
+print(res)
 
 i = 0
 d = {}
+
 while i < len(res):
     d[res[i]] = data.count(data[i])
     i += data.count(data[i])
@@ -30,3 +40,4 @@ d = sorted(d.items(), key=lambda item: item[1], reverse=True)
 #print(d)
 df = pd.DataFrame(d)
 df.to_csv("res.csv", mode='a', index= False, encoding='utf-8')
+print('Check res.csv')
