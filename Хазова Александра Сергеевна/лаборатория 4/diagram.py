1921721dc0aa720622fc7544e6c421874bb3b2b4
Хазова Александра <72195557+Khazovalexandra@users.noreply.github.com>
2022-12-11 17:13:11 +0300

    [4] самые популярные слова в названиях график

--- Хазова Александра Сергеевна/лаборатория 4/diagram.py
+++ Хазова Александра Сергеевна/лаборатория 4/diagram.py
@@ -3,30 +3,28 @@ import matplotlib.pyplot as plt
 
 data = []
 data1 = []
-#for l in open(r'C:\Users\Сергей\Documents\parsing\res.csv', 'r', encoding="utf8").readlines()[1:]:
-    #d = str(l.split(',')[0])
-    #d = d.replace('"', '')
-    #d = d.replace(".", "")
-    #d = d.replace("?", "")
-    #d = d.replace(")", "")
-    #d = d.replace("(", "")
-    #d = d.replace(":", "")
-    #d = d.replace("!", "")
-    #d = d.replace("»", "")
-    #d = d.replace("«", "")
-    #d = d.replace("\\n","")
-    #data.append(d)
+w = lambda x: (x.replace('.','').replace('"', "").replace("?", "").replace(")", "").replace("(", "").replace(":", "").replace("!", "").replace("»", "").replace("«", ""))
 
-df = pd.read_csv("res.csv")
+df = pd.read_csv("res.csv", converters={'0':w})
 df1 = df.sort_values(by=['0'], ascending=False)
 s = df['0'].loc[df.index[0]]
+
 plt.text(0, 350,"Самое распространненое слово: " + s)
+
 plt.grid()
-data = list(df1['0'])
-data1 = list(df1['1'])
-plt.title("Повторение слов в файле")
+#data = list(df1['0'])
+#data1 = list(df1['1'])
+
+words = list(filter(lambda x:x>=50, df1['1']))
+k = df.loc[lambda df: df['1'] >= 50, "0"]
+
+plt.title("Повторение слов в файле(>=50)")
 plt.xlabel("Слова")
-plt.plot(data1)
-#plt.pie(data1)
+plt.ylabel("Количество повторений")
+plt.tick_params(axis='x', rotation=90)
+
+#plt.plot(data1)
+plt.plot(k , words, 'm')
+#plt.pie(words)
 #plt.plot(data, data1)
 plt.show()
