7427190be29dc4b507316ad4a191f8bdfd5cd355
Joofer <joofer.sp@gmail.com>
2023-01-09 17:17:19 +0300

    [4]

--- Лоскутов Андрей Викторович/_lab-4/count.py
+++ Лоскутов Андрей Викторович/_lab-4/count.py
@@ -4,6 +4,7 @@ from collections import Counter
 data = []
 with open(r'Лоскутов Андрей Викторович\result.csv','r',encoding='utf-8') as file:
     data = file.readlines()
+    
 for i in range(len(data)):
     data[i] = re.split('\W',data[i])
 _data = []
@@ -13,5 +14,6 @@ for i in data:
 _data = [e for e in _data if e]
 res = dict(Counter(_data))
 res1 = {k: v for k, v in sorted(res.items(), key=lambda item: item[1],reverse=True)}
+
 with open(r'Лоскутов Андрей Викторович\final.txt','w',encoding='utf-8') as file:
     file.write(str(res1))
