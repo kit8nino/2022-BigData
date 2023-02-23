b4f9b0203d8e246bcc967c4e749a968a125fb685
Хазова Александра <khazovalexandra@gmail.com>
2023-02-23 21:13:58 +0300

    [5]

--- Хазова Александра Сергеевна/интеллектуальный анализ/lab5.py
+++ Хазова Александра Сергеевна/интеллектуальный анализ/lab5.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv('survey.csv')
+df = pd.read_csv(r'..\интеллектуальный анализ\survey.csv')
 # https://www.kaggle.com/datasets/osmi/mental-health-in-tech-survey
 
 """
