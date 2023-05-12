88af3011c847439b02e4ea89a5b45077168d8438
kit8nino <freezer.nik@gmail.com>
2023-05-12 11:56:04 +0300

    [6] sorted images

--- _lab-6/resize_img.py
+++ _lab-6/resize_img.py
@@ -1,9 +1,12 @@
 from PIL import Image
 import os, sys
+import pandas as pd
+
 
 path = "movies_posters/"
 dirs = os.listdir(path)
-resize_ratio = 0.2  # where 0.5 is half size, 2 is double size
+resize_ratio = .2  # where 0.5 is half size, 2 is double size
+df = pd.read_csv('movies_dataset.csv')
 
 
 def resize_aspect_fit():
@@ -18,8 +21,20 @@ def resize_aspect_fit():
             new_image_height = int(image.size[0] / (1/resize_ratio))
             new_image_length = int(image.size[1] / (1/resize_ratio))
 
-            image = image.resize((new_image_height, new_image_length), Image.ANTIALIAS)
-            image.save('resized/' + file_path + extension, 'JPEG', quality=90)
+            image = image.resize((new_image_height, new_image_length),
+                                 Image.ANTIALIAS)
+
+            folder, img_id = file_path.split('/')
+            genre = df.loc[df['id'] == img_id, 'genres_list'].apply(eval)
+            try:
+                genre = genre.tolist()[0][0]
+            except:
+                genre = 'Mystery'
+            new_folder = folder + '/' + genre
+            if not os.path.exists(new_folder):
+                os.mkdir(new_folder)
+            image.save(folder + '/' + genre + '/' + img_id +
+                       extension, 'JPEG', quality=90)
 
 
 resize_aspect_fit()
