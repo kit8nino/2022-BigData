cf2b971b8cce04351870b4de0ae9232598cc2cb7
daryabobina <72434096+daryabobina@users.noreply.github.com>
2023-06-08 00:02:29 +0300

    Update main.py

--- Бобина Дарья/[5] lab5 ia/main.py
+++ Бобина Дарья/[5] lab5 ia/main.py
@@ -1,131 +1,191 @@
-import os
+from functools import partial
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
-import pathlib
-import tensorflow as tf
-from tensorflow import keras
-from keras import layers
+import numpy as np
+import json
 
+folder = r'C:\\Users\\dasha\\pythonProject5\\'
+folderPlots = 'plots\\'
+dfName = 'newsurvey.csv'
 
-data_dir = pathlib.Path('movies_posters')
-genres = pd.read_csv('movies_dataset.csv')
+def WhatToDo():
+    return input('1 - переписать файл\n2 - статистический анализ данных\n3 - ключевые признаки наличия заболевания'
+                 '\n4 - графики для ключевых признаков\n5 - теорема Байеса\n  Ваш покемон: ')
 
-genres_set = set(genres['genres_list'])
-genres_dict = dict(zip(genres_set, range(len(genres_set))))
-path = 'what'
+def RewriteCsv():
+    df = pd.read_csv(folder+'survey.csv')
 
-img_height = 56
-img_width = 38
-batch_size = 32
-epoch = 0
-PATH = path + f'checkpoint_{epoch}.ckpt'
+    def ValueToInt(x, dict, name):
+        if x in dict:
+            df.loc[df[name] == x, name] = dict[x]
 
+    def ValueToIntYesNo(x, name):
+        if x != 0 and x!=1:
+            df.loc[df[name] == x, name] = 0 if (x=='No') else (1 if (x=='Yes') else np.NaN)
 
-def load_imgs(data_dir=data_dir):
-    image_count = len(list(data_dir.glob('*/*.jpg')))
-    list_ds = tf.data.Dataset.list_files(str(data_dir/'*'), shuffle=True)
-    #list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
-    return list_ds, image_count
+    def Age():
+        df['Age'].mask((df['Age'] > 100) | (df['Age'] < 15), inplace=True)
 
+    def Gender():
+        d1 = pd.Series(map(lambda x: 1 if (
+                (x == "M") or (x == "Male") or (x == "m") or (x == "male") or (x == "Mal") or (
+                x == "Male ") or (x == "Man") or (x == "Male-ish") or (x == "maile") or (
+                        x == "Cis Male") or (x == "Male (CIS)") or (x == "Make") or (x == "msle") or (
+                        x == "cis male") or (x == "Cis Male") or (x == "Malr") or (x == "Mail")) else x,
+                           df.Gender))
+        d2 = pd.Series(map(lambda x: 0 if (
+                (x == "female") or (x == "F") or (x == "Woman") or (x == "f") or (x == "woman") or (
+                x == "Female") or (x == "Cis Female") or (x == "Femake") or (x == "cis-female/femme") or (
+                        x == "Femail (cis)") or (x == "femail")) else 2, d1))
 
-def get_label(file_path, genres=genres):
-    parts = tf.strings.split(file_path, sep=os.path.sep)
-    img_id = tf.strings.split(parts[-1], '.')[-2]
-    # g_list = list(genres[genres['id'] == img_id['genres_list']])
-    # g_indeces = [genres_dict[x] for x in g_list]
-    return int(genres.loc[genres['id'] == img_id, 'genres_list'].iloc[0][:4])
+    def Leave():
+        name='leave'
+        dict={'Very easy': 0,'Somewhat easy': 1, 'Don\'t know': 2,'Somewhat difficult': 3,'Very difficult': 4}
+        mapfunc = partial(ValueToInt, dict=dict, name=name)
+        pd.Series(map(mapfunc, df[name]), df[name])
 
-def get_id(file_path):
-    parts = tf.strings.split(file_path, sep=os.path.sep)
-    img_id = tf.strings.split(parts[-1], '.')[-2]
-    return img_id
+    def No_employees():
+        name='no_employees'
+        dict={'1-5': 0,'6-25': 1, '26-100': 2,'100-500': 3, '500-1000': 4,'More than 1000': 5}
+        mapfunc = partial(ValueToInt, dict=dict, name=name)
+        pd.Series(map(mapfunc, df[name]), df[name])
 
-def decode_img(img, img_height=img_height, img_width=img_width):
-    img = tf.io.decode_jpeg(img, channels=3)
-    return tf.image.resize(img, [img_height, img_width])
+    def Work_interfere():
+        name='work_interfere'
+        dict={'Never': 0,'Rarely': 1, 'Sometimes': 2,'Often': 3}
+        mapfunc = partial(ValueToInt, dict=dict, name=name)
+        pd.Series(map(mapfunc, df[name]), df[name])
 
-def process_path(file_path):
-    label = get_label(file_path)
-    img = tf.io.read_file(file_path)
-    img = decode_img(img)
-    return img, label
+    def YesNo():
+        dfNamesYesNo = ['self_employed','family_history', 'treatment', 'remote_work', 'tech_company', 'benefits',
+                        'care_options', 'wellness_program', 'seek_help', 'anonymity', 'mental_health_consequence',
+                        'phys_health_consequence', 'coworkers', 'supervisor', 'mental_health_interview',
+                        'phys_health_interview', 'mental_vs_physical', 'obs_consequence']
+        for name in dfNamesYesNo:
+            mapfunc = partial(ValueToIntYesNo, name=name)
+            pd.Series(map(mapfunc, df[name]))
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size,
-    label_mode='categorical')
+    def CallFuncs():
+        Age()
+        Gender()
+        Leave()
+        No_employees()
+        Work_interfere()
+        YesNo()
 
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size,
-    label_mode='categorical')
+    CallFuncs()
+    print(df)
+    df.to_csv(folder+dfName, mode='a', index= False, encoding='utf-8')
 
+def Statistic():
+    df = pd.read_csv(folder+dfName).drop(columns=['Timestamp', 'comments', 'Country', 'state'])
+    seek = df[~df['treatment'].isnull() & df['treatment'] == 1]
+    dont = df[~df['treatment'].isnull() & df['treatment'] == 0]
+    print(df.info())
+    print('ВСЕ')
+    print(df.describe(include='all'))
+    print('БОЛЬНЫЕ')
+    print(seek.describe())
+    print(seek.value_counts(dropna=False, normalize=True))
+    print(seek.var(ddof=1))
+    print('ЗДОРОВЫЕ')
+    print(dont.describe())
+    print(dont.value_counts(dropna=False, normalize=True))
+    print(dont.var(ddof=1))
+    print('ОБЩАЯ СТАТИСТИКА')
+    ft_names= dont.columns.tolist()
 
+    for column in ft_names:
+        if column == 'Country' or column == 'state' or column == 'Gender':
+            continue
+        avg =  ', avg: '+ str(df['Age'].sum()/df['Age'].value_counts(dropna=False).sum()) if column == 'Age' else ''
+        expectedValue=str(sum(df[column].value_counts(dropna=False)*df[column].value_counts(dropna=False, normalize=True)))
+        print(str(df[column].value_counts(dropna=False, normalize=True)) + f', dispersion: {df[column].var()}, expected value: {expectedValue}'+avg)
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Normalization(input_shape=(img_height, img_width, 3),
-                                mean=510, variance=255),
-    tf.keras.layers.Conv2D(filters=512, kernel_size=(4, 4),
-                        padding='same', activation='relu',
-                        strides=(2, 2)),
-    tf.keras.layers.MaxPool2D(pool_size=2, strides=None,
-                            padding='valid',
-                            data_format='channels_last'),
-    tf.keras.layers.Conv2D(filters=256, kernel_size=(4, 4),
-                        padding='same', activation='relu',
-                        strides=(2, 2)),
-    tf.keras.layers.MaxPool2D(pool_size=2, strides=None,
-                            padding='valid',
-                            data_format='channels_last'),
-    tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3),
-                        padding='same', activation='relu',
-                        strides=(1, 1)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dropout(.5),
-    tf.keras.layers.Dense(64, activation='sigmoid'),
-    tf.keras.layers.Dense(32, activation='sigmoid'),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
+def Correlation(task):
+    df = pd.read_csv(folder+dfName)
+    df = df.drop(columns=['Country', 'state', 'comments', 'Timestamp'])
+    corrmap = df.corr(numeric_only = True)
+    cols = corrmap.nlargest(10, 'treatment')['treatment'].index
+    corrTreatment = np.corrcoef(corrmap[cols].values, rowvar=False)
 
+    def CorrPlt():
+        plt.figure(figsize=(12,8) , dpi=100)
+        sns.heatmap(corrmap, vmax=.8, square=True)
+        plt.savefig(folder+folderPlots+'2 corr.png')
+        plt.show()
 
-model.compile(optimizer='adam', loss='categorical_crossentropy',
-            metrics=tf.keras.metrics.CategoricalAccuracy(),
-            run_eagerly=True)
+    def TreatmentPlt():
+        plt.figure(figsize=(12,8) , dpi=100)
+        sns.heatmap(corrTreatment, annot=True, vmax=.8, square=True, annot_kws={'size':9}, yticklabels=cols.values, xticklabels=cols.values, color=['red','blue'])
+        plt.savefig(folder+folderPlots+'2 treatment.png')
+        plt.show()
 
-# Define the checkpoint callback that saves the model's weights at every epoch
+    def PlotGraphs():
+        for i in cols.tolist():
+            if i == 'treatment': continue
+            dfNew= pd.DataFrame({'seek': pd.Series(df[~df['treatment'].isnull() & df['treatment'] == 1].groupby(i).size()), 'dont': pd.Series(df[~df['treatment'].isnull() & df['treatment'] == 0].groupby(i).size())})
+            ax = dfNew.plot(kind='bar', stacked= True , color=['lime', 'green'], mark_right = True)
+            for p in ax.patches:
+                ax.annotate(str(p.get_height()), (p.get_x() * 1.001, p.get_y() + 5))
+            plt.savefig(folder+folderPlots+'3 ' + i+'.png')
+            plt.show()
 
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-                            filepath=PATH,
-                            save_weights_only=True, # If False, full model
-                            save_freq='epoch')
+    if (task == 3):
+        CorrPlt()
+        TreatmentPlt()
+    elif (task == 4):
+        PlotGraphs()
 
-model.summary()
-input("Press enter to start")
+def Probability():
+    df = pd.read_csv(folder+dfName)
+    df = df.drop(columns=['Country', 'state', 'comments', 'Timestamp'])
+    dfSeek = df[~df['treatment'].isnull() & df['treatment'] == 1]
+    corrmap = df.corr(numeric_only = True)
+    cols = corrmap.nlargest(10, 'treatment')['treatment'].index
 
+    def СalcP(name):
+        P=[]
+        tot = []
+        maxim = int(df[name].max())+1
+        p = list((df.groupby(name).size() / len(df)).tolist())
+        pSeek = list((dfSeek.groupby(name).size() / len(dfSeek)).tolist())
 
-hist = model.fit(train_ds,
-                validation_freq=[2, 5, 9],
-                epochs=3,
-                callbacks=[cp_callback],
-                use_multiprocessing=True)
+        for i in range(0, maxim):
+             tot.append(p[i]*pSeek[i])
+        for i in range(0, maxim):
+            P.append(pSeek[i]/sum(pSeek))
+            print(f'При ответе {i}: {round(P[i]*100.0, 2)}%')
+        return(P, pSeek)
 
-metrics_df = pd.DataFrame(hist.history)
-print(metrics_df)
+    print('Выделенные поля: ', cols.tolist()[1:10])
+    print('ВЕРОЯТНОСТЬ, что при данном ответе на вопрос человеку будет нужна психологическая помощь')
+    prob = {}
+    maxPSeek = []
+    totPSeek = []
+    indexPSeek = []
 
-metrics_df["loss"].plot()
-plt.show()
+    for name in cols.tolist()[1:10]:
+        print(f'Вопрос: {name}')
+        prob[name] = СalcP(name)
+        index = prob.get(name)[0].index(max(prob.get(name)[0]))
+        indexPSeek.append(f'{name}: {index}')
+        maxPSeek.append(prob.get(name)[1][index])
+        totPSeek.append(sum(prob.get(name)[1]))
+        print()
 
-metrics_df["categorical_accuracy"].plot()
-plt.show()
+    print(json.dumps(prob, indent=4, sort_keys=False))
+    print(f'\nВЕРОЯТНОСТЬ, что при данных ответах на вопросы\n{indexPSeek}\nчеловеку будет нужна психологическая помощь: {sum(maxPSeek)/sum(totPSeek)},\nотносительно других вариантов ответа на эти вопросы')
 
-#plt.savefig(path + 'loss')
-#plt.savefig(path + 'categorical_accuracy')
-
+whatToDo = WhatToDo()
+if whatToDo == '1':
+    RewriteCsv()
+elif whatToDo == '2':
+    Statistic()
+elif whatToDo == '3':
+    Correlation(3)
+elif whatToDo == '4':
+    Correlation(4)
+elif whatToDo == '5':
+    Probability()
